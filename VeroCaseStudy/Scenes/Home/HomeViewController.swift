5c248f9e9fa10a2648425a0157f2ae7eba8df12a
burak cevizli <burak.cevizli7@gmail.com>
2023-04-01 20:12:05 +0300

    QR Scanner and Pull-to-Refresh Functionality

--- VeroCaseStudy/Scenes/Home/HomeViewController.swift
+++ VeroCaseStudy/Scenes/Home/HomeViewController.swift
@@ -7,16 +7,16 @@
 
 import UIKit
 import Alamofire
+import AVFoundation
 
 class HomeViewController: UIViewController {
     
     let tableView = UITableView()
-    
     var tasksArray = [Items]()
-    
     var viewModel: HomeViewModel!
-    
     var button: UIButton!
+    let qrCodeImageView = UIImageView(image: UIImage(named: "qrLogo"))
+    var refreshControl: UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +24,36 @@ class HomeViewController: UIViewController {
         createTableView()
         searchBar()
         loginButton()
+        qrScan()
+    }
+    
+    private func qrScan() {
+        // Add the QR code image view to the view
+        view.addSubview(qrCodeImageView)
+        
+        // Add a tap gesture recognizer to the QR code image view
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(openQRScanner))
+        qrCodeImageView.addGestureRecognizer(tapGesture)
+        qrCodeImageView.isUserInteractionEnabled = true
+        qrCodeImageView.layer.cornerRadius = 20
+        qrCodeImageView.clipsToBounds = true
+        qrCodeImageView.sizeToFit()
+        qrCodeImageView.isHidden = true
+        
+        // Position the QR code image view in the bottom right corner of the view
+        qrCodeImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            qrCodeImageView.widthAnchor.constraint(equalToConstant: 74),
+            qrCodeImageView.heightAnchor.constraint(equalToConstant: 74),
+            qrCodeImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -26),
+            qrCodeImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -26)
+        ])
+    }
+    @objc func openQRScanner() {
+        let qrScannerVC = QRScannerViewController()
+        qrScannerVC.modalPresentationStyle = .fullScreen
+        present(qrScannerVC, animated: true, completion: nil)
     }
     
     private func loginButton() {
@@ -36,7 +66,7 @@ class HomeViewController: UIViewController {
         view.addSubview(button)
         
         button.anchorWithCenter(centerX: view.centerXAnchor, centerY: view.centerYAnchor, size: CGSize(width: 120, height: 50))
-
+        
         button.addTarget(self, action: #selector(didLoginTapped), for: .touchUpInside)
     }
     @objc private func didLoginTapped() {
@@ -49,6 +79,7 @@ class HomeViewController: UIViewController {
             }
         }
         button.removeFromSuperview()
+        qrCodeImageView.isHidden = false
     }
     private func createTableView() {
         
@@ -56,14 +87,21 @@ class HomeViewController: UIViewController {
         tableView.dataSource = self
         
         tableView.estimatedRowHeight = 50
-                
         view.addSubview(tableView)
         
         tableView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, padding: UIEdgeInsets(top: 0, left: 10, bottom: 0, right: -10))
-        
         tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.identifier)
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+        tableView.addSubview(refreshControl)
     }
-    
+    @objc func refreshData() {
+           
+           // Reload the table view and end refreshing
+           tableView.reloadData()
+           refreshControl.endRefreshing()
+       }
 }
 
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
@@ -118,3 +156,5 @@ extension HomeViewController: UISearchBarDelegate {
         self.tableView.reloadData()
     }
 }
+
+
---
+++ VeroCaseStudy/Scenes/Scanner/QRScannerViewController.swift
@@ -0,0 +1,104 @@
+//
+//  QRScannerViewController.swift
+//  VeroCaseStudy
+//
+//  Created by Adem Burak Cevizli on 1.04.2023.
+//
+
+import UIKit
+import AVFoundation
+
+class QRScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+
+    // AVCapture variables
+    private var captureSession: AVCaptureSession?
+    private var capturePreviewLayer: AVCaptureVideoPreviewLayer?
+    private var qrCodeFrameView: UIView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup capture session
+        setupCaptureSession()
+
+        // Add capture preview layer
+        setupPreviewLayer()
+
+        // Add bounding box view for detected QR code
+        setupQRCodeFrameView()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Stop capturing data when view disappears
+        captureSession?.stopRunning()
+    }
+
+    private func setupCaptureSession() {
+        // Create capture session
+        captureSession = AVCaptureSession()
+
+        // Set up default capture device
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
+            fatalError("No video device found")
+        }
+
+        // Create capture input from default device
+        guard let captureInput = try? AVCaptureDeviceInput(device: captureDevice) else {
+            fatalError("Unable to create input from capture device")
+        }
+
+        // Add capture input to session
+        if captureSession?.canAddInput(captureInput) == true {
+            captureSession?.addInput(captureInput)
+        }
+
+        // Create metadata output and add to session
+        let captureOutput = AVCaptureMetadataOutput()
+        if captureSession?.canAddOutput(captureOutput) == true {
+            captureSession?.addOutput(captureOutput)
+        }
+
+        // Set metadata output delegate and types
+        captureOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        captureOutput.metadataObjectTypes = [.qr]
+    }
+
+    private func setupPreviewLayer() {
+        // Create preview layer and add to view
+        capturePreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+        capturePreviewLayer?.videoGravity = .resizeAspectFill
+        capturePreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(capturePreviewLayer!)
+
+        // Start capturing data
+        captureSession?.startRunning()
+    }
+
+    private func setupQRCodeFrameView() {
+        // Create bounding box view for detected QR code
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.green.cgColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubviewToFront(qrCodeFrameView!)
+    }
+
+    // AVCaptureMetadataOutputObjectsDelegate method
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadata object is a QR code
+        guard let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject, metadataObject.type == .qr, let stringValue = metadataObject.stringValue else {
+                qrCodeFrameView?.frame = .zero
+                return
+            }
+
+            // Draw bounding box around detected QR code
+            let transformedRect = capturePreviewLayer?.transformedMetadataObject(for: metadataObject)?.bounds ?? .zero
+            let convertedRect = capturePreviewLayer?.layerRectConverted(fromMetadataOutputRect: transformedRect) ?? .zero
+            qrCodeFrameView?.frame = convertedRect
+
+            // Do something with the scanned string value
+            print("Scanned QR code with value: \(stringValue)")
+        }
+
+}
